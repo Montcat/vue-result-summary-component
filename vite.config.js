56f2d410735857b9752f1c8724b65b2d1b88d660
Montcat <53400239+Montcat@users.noreply.github.com>
2023-04-13 17:23:18 +0000

    added some comments

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-result-summary-component/",
+  base: "/vue-result-summary-component/", //the base directory for the gh-pages to deploy from.
   plugins: [vue()],
   resolve: {
     alias: {
