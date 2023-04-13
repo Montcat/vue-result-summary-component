0423efd502cca75b9eff17d1a68957e82f9f07e9
Montcat <53400239+Montcat@users.noreply.github.com>
2023-04-13 17:06:54 +0000

    initial commit

---
+++ project/README-template.md
@@ -0,0 +1,92 @@
+# Frontend Mentor - Results summary component solution
+
+This is a solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### Screenshot
+
+#### The Mobile View:
+
+1. The mobile view screenshot is located in the 'project/screenshots' folder taken with firefox
+2. The design guide's max-width for the mobile device was a bit to small for my project so made a few adjustments.
+
+#### The Desktop View:
+
+1. After I was done with this project, I found out through my research that I was supposed to design the mobile view first but any way, the desktop design was not too challenging for me on this project.
+
+### Links
+
+- Solution URL: [https://github.com/Montcat/results-summary-component]
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Desktop-first workflow
+- [Vue](https://vuejs.org/) - JS framework
+
+### What I learned
+
+Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+To see how you can add code snippets, see below:
+
+```html
+<span>Some HTML code I'm proud of</span>
+```
+
+```css
+/* I learnt how to use variables in css during my research and also using linear gradients on background and the 'hsla()' property */
+:root {
+	--variable-name: hsla();
+}
+```
+
+```js
+// Did not use any funtions in JavaScript in this project
+```
+
+### Continued development
+
+Use this section to outline areas that you want to continue focusing on in future projects. These could be concepts you're still not completely comfortable with or techniques you found useful that you want to refine and perfect.
+
+- I'd love to improve on my skills with SCSS(sass) because searching for selectors using pure css is very tiring and fraustrating.
+
+- I want to improve my understanding on positioning elements with the absolute, fixed and sticky. These concepts even though understandable but implementing them is not easy you would have to make frequent check on your ui to see if anything has broken.
+
+- Some colors I couldn't get them, I think it's because of the picture quality thats why it seems so different from mine.
+
+- And also, I have not seen how the figma documents works to make productivity easier but would see if I can get one project that comes with a figma doc so I can see the difference.
+
+### Useful resources
+
+- [Linear Gradient Background](https://www.w3schools.com) - This example was very simple and very understandable. I loved the way it was explained.
+
+- [hsla()](https://www.w3schools.com) - This also came from the same website, it was free but very understandable though.
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@Montcat](https://www.frontendmentor.io/profile/Montcat)
+
+## Acknowledgments
+
+- Unfortunately, did not recieve help from anyone during this project, hope I can get a mentor to help me on my journey of becoming a full-scale frontend developer. From where I come from, Accra - Ghana, it's very difficult to find a mentor that is willing to help you grow your knowledge in programming. And please can't wait to get my feedback, let me know if there is anything that I have to add.
---
+++ src/App.vue
@@ -0,0 +1,235 @@
+<template>
+	<div class="container">
+		<div class="card-container">
+			<div class="cards left">
+				<p>Your Result</p>
+				<div class="result">
+					<span class="score">76</span>
+					<span class="percentage">of 100</span>
+				</div>
+				<p>Great</p>
+				<p>
+					You scored higher than 65% of the people who have taken these tests
+				</p>
+			</div>
+			<div class="cards right">
+				<h3>Summary</h3>
+				<div class="summary-container">
+					<div class="summary-item">
+						<span class="summary-subject">
+							<span class="subject-icon"
+								><img src="./components/icons/bolt.svg" alt=""
+							/></span>
+							Reaction</span
+						>
+						<span class="summary-score">
+							<span class="summary-score-percentage">80</span> / 100
+						</span>
+					</div>
+					<div class="summary-item">
+						<span class="summary-subject">
+							<span class="subject-icon"
+								><img src="./components/icons/brain.svg" alt="" /></span
+							>Memory
+						</span>
+						<span class="summary-score">
+							<span class="summary-score-percentage">92</span> / 100
+						</span>
+					</div>
+					<div class="summary-item">
+						<span class="summary-subject">
+							<span class="subject-icon"
+								><img src="./components/icons/verbal.svg" alt="" /></span
+							>Verbal</span
+						>
+						<span class="summary-score">
+							<span class="summary-score-percentage">61</span> / 100
+						</span>
+					</div>
+					<div class="summary-item">
+						<span class="summary-subject">
+							<span class="subject-icon"
+								><img src="./components/icons/visual.svg" alt="" /></span
+							>Visual</span
+						>
+						<span class="summary-score">
+							<span class="summary-score-percentage">72</span> / 100
+						</span>
+					</div>
+				</div>
+				<button>Continue</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+	.container {
+		padding: 20%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.card-container {
+		display: flex;
+		box-shadow: 28px 38px 32px -33px var(--neutral-light-lavender);
+		border-radius: 3rem;
+	}
+
+	.card-container > .cards {
+		width: 24rem;
+	}
+
+	.card-container > .left {
+		background: linear-gradient(
+			var(--light-slate-blue),
+			var(--light-royal-blue)
+		);
+		border-radius: 3rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.card-container > .left > p:first-child {
+		color: hsl(241, 100%, 89%);
+		font-weight: 500;
+		font-size: 1.5rem;
+		margin: 1rem 0;
+	}
+
+	.left > .result {
+		background: linear-gradient(var(--violet-blue), var(--persian-blue));
+		height: 14rem;
+		width: 14rem;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-radius: 50%;
+	}
+
+	.score {
+		color: var(--neutral-white);
+		font-size: 4rem;
+		font-weight: 900;
+		height: 4rem;
+		margin-bottom: 1rem;
+	}
+
+	.percentage {
+		color: var(--neutral-light-lavender);
+	}
+
+	div.cards:nth-child(1) > p:nth-child(3) {
+		color: var(--neutral-white);
+		font-size: 2rem;
+		margin: 1rem 0;
+		font-weight: 700;
+	}
+
+	div.cards:nth-child(1) > p:nth-child(4) {
+		text-align: center;
+		height: 7rem;
+		padding: 0 16%;
+		color: var(--neutral-light-lavender);
+	}
+
+	.right {
+		padding: 2rem 2rem;
+	}
+
+	.right > h3 {
+		margin: 1rem 0;
+		color: var(--neutral-dark-gray-blue);
+		font-size: 1.5rem;
+		font-weight: 700;
+	}
+
+	.summary-item {
+		margin: 0 0 1rem 0;
+		padding: 0.5rem 1rem;
+		display: flex;
+		justify-content: space-between;
+		height: 4rem;
+		align-items: center;
+		border-radius: 0.5rem;
+	}
+
+	/*Subject- Reaction */
+	.summary-item:nth-child(1) {
+		background: hsla(0, 100%, 67%, 0.1);
+	}
+
+	.summary-item > span.summary-subject {
+		font-weight: 700;
+		display: flex;
+	}
+
+	div.summary-item:nth-child(1) > span:nth-child(1) {
+		color: var(--light-red);
+	}
+
+	.subject-icon {
+		display: flex;
+		margin-right: 0.2rem;
+	}
+
+	/*Subject- Memory */
+	div.summary-item:nth-child(2) {
+		background: hsla(38.8, 100%, 56.1%, 0.1);
+	}
+
+	div.summary-item:nth-child(2) > span:nth-child(1) {
+		color: var(--orange);
+	}
+
+	/*Subject- Verbal */
+	div.summary-item:nth-child(3) {
+		background: hsla(166, 100%, 37.1%, 0.1);
+	}
+
+	div.summary-item:nth-child(3) > span:nth-child(1) {
+		color: var(--green-teal);
+	}
+
+	/*Subject- Verbal */
+	div.summary-item:nth-child(4) {
+		background: hsla(233.8, 85.2%, 44.9%, 0.1);
+	}
+
+	div.summary-item:nth-child(4) > span:nth-child(1) {
+		color: var(--cobalt-blue);
+	}
+
+	/**summary score section */
+	.summary-score-percentage {
+		font-weight: 700;
+		color: var(--neutral-dark-gray-blue);
+	}
+
+	.summary-score {
+		color: var(--neutral-light-lavender);
+	}
+
+	/**button */
+	div.cards:nth-child(2) > button:nth-child(3) {
+		width: 100%;
+		height: 3rem;
+		border-radius: 2em;
+		background: var(--neutral-dark-gray-blue);
+		color: var(--neutral-pale-blue);
+		font-weight: 700;
+		margin-top: 1rem;
+	}
+
+	@media only screen and (max-width: 429px) {
+		.card-container {
+			flex-direction: column;
+		}
+	}
+</style>
